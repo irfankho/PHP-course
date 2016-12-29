e87e0e274c442f2cd5e0966560c778d5937dea5b
Irfaan <irfaankhodabukus@gmail.com>
2016-12-29 10:51:16 +0100

    php introduction

---
+++ fordperfect.php
@@ -0,0 +1,16 @@
+<?php
+
+function ford_Perfect()
+{
+	$tab = ['Le Guide du voyageur galactique', 'Le Dernier Restaurant avant la fin du monde', 'La Vie, l\'Univers et le Reste', 'Salut, et encore merci pour le poisson', 'Globalement inoffensive'];
+	foreach ($tab as $livre) 
+	{
+		echo $livre.'<br>';
+	}
+}
+
+ford_Perfect();
+
+
+
+
