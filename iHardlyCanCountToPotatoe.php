0649bf6def39cb16cc797793b474c04156f90145
Irfaan <irfaankhodabukus@gmail.com>
2016-12-29 11:19:22 +0100

    php introduction

---
+++ iHardlyCanCountToPotatoe.php
@@ -0,0 +1,17 @@
+<?php
+// need to test if the parameter number exists
+$limit = $_GET['number']
+
+$count = 0;
+
+while($count <= $limit)
+{
+	echo $count.'<br>';
+	$count = $count + 1;
+}
+
+for($count = 0; $count <= $limit; $count++)
+}
+	echo $count.'<br>';
+}
+
